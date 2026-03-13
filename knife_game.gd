beb9b5a8265fbc4ec7525f5a5d5a1f7c04df05d5
Grom Chavkerov <gromcha4@gmail.com>
2026-03-12 16:27:31 -0800

    Update knife_game.gd

--- knife_game.gd
+++ knife_game.gd
@@ -5,22 +5,26 @@ extends Node2D
 @onready var currency_amount: Label = $CurrencyAmount
 
 
-var needed_keys = ["a", "space", "w", "space", "s", "space", "d", "space", "end"]
+#var needed_keys = ["a", "space", "w", "space", "s", "space", "d", "space", "end"]
+var needed_keys = []
 var hurt_keys = ["a", "w", "s", "d", "space"]
+var random_key_options = ["a", "w", "s", "d"]
 var current_needed_key = 0
-var space_needed = false
+var needed_keys_length = 8
+
 
 
 func _ready() -> void:
-	current_key_needed_label.text = needed_keys[current_needed_key]
+	randomize_keys()
 
 
 func _process(_delta: float) -> void:
-	if needed_keys[current_needed_key] == needed_keys[-1]:
+	if current_needed_key == needed_keys_length:
 		current_needed_key = 0
 		current_key_needed_label.text = needed_keys[current_needed_key]
 		Game.currency += 10
 		currency_amount.text = str(Game.currency)
+		randomize_keys()
 
 	elif needed_keys.size() - 1 >= current_needed_key:
 		if Input.is_action_just_pressed(needed_keys[current_needed_key]):
@@ -31,3 +35,13 @@ func _process(_delta: float) -> void:
 			for i in hurt_keys:
 				if Input.is_action_just_pressed(i):
 					print("ouchie")
+
+
+func randomize_keys():
+	needed_keys = []
+	while needed_keys.size() < needed_keys_length:
+		needed_keys.append(random_key_options[randi_range(0, random_key_options.size() - 1)])
+		needed_keys.append("space")
+	needed_keys.append("end")
+	current_key_needed_label.text = needed_keys[current_needed_key]
+	print(needed_keys)
