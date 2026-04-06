9cb7f8faa030084cdb2768b65cbef94083ef8e3d
Grom Chavkerov <gromcha4@gmail.com>
2026-04-06 12:45:49 -1000

    Create win_lose_screen.gd

---
+++ win_lose_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var label: Label = $CanvasLayer/HBoxContainer/Label
+
+func _ready() -> void:
+	if Game.win:
+		label.text = "YOU WIN!!!"
+		Game.win = null
+	else:
+		label.text = "YOU LOSE!!!"
+		Game.win = null
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
