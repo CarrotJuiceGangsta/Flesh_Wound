01dafeeb480996a55bd268623154d2875dc1cee7
Grom Chavkerov <gromcha4@gmail.com>
2026-04-08 16:45:11 -1000

    Update ring_resource.gd

--- ring_resource.gd
+++ ring_resource.gd
@@ -12,6 +12,13 @@ class_name ring_type
 @export var reward_amount : int
 @export var scales : bool
 @export var trigger : triggers
+@export var dies : bool
+@export var activation_time : int
+@export var death_time : int
+
+var activations = 0
+var death_activations = 0
+var dead = false
 
 enum rarities {
 	COMMON,
@@ -32,10 +39,14 @@ enum triggers {
 
 
 func action():
-	if reward == rewards.CURRENCY:
-		Game.currency += reward_amount
-	if reward == rewards.POINTS:
-		Game.current_score += reward_amount
-	if scales == true:
-		reward_amount += 1
-	print(reward_amount)
+	if activations >= activation_time and !dead:
+		if death_activations < death_time or !dies:
+			if reward == rewards.CURRENCY:
+				Game.currency += reward_amount
+			if reward == rewards.POINTS:
+				Game.current_score += reward_amount
+			if scales == true:
+				reward_amount += 1
+		else:
+			if dies == true:
+				dead = true
