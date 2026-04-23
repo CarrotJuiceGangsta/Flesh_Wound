e2a0b8e722c90e7f695ce1f68bd2944909598ba1
Grom Chavkerov <gromcha4@gmail.com>
2026-04-23 12:32:19 -1000

    Update game.gd

--- game.gd
+++ game.gd
@@ -4,8 +4,10 @@ const BAMBOO_RING = preload("uid://c540rjjt0ewjh")
 const DIAMOND_RING = preload("uid://guhq4mubk0pr")
 const FLOWER_RING = preload("uid://b3hwix1pm08dm")
 const GOLD_RING = preload("uid://dikicfnmindx8")
+const SILVER_RING = preload("uid://dlqt56shlagc0")
 
 
+var dragging = false
 
 var current_score = 0
 
