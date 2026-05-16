0d68ef938ef55ef6e9eb18b04ba6484bfa33aa75
Grom Chavkerov <gromcha4@gmail.com>
2026-05-15 17:23:34 -1000

    Update ring_base.gd

--- ring_base.gd
+++ ring_base.gd
@@ -7,6 +7,8 @@ var ring_slot_4_position = Vector2(1350.0, 500.0)
 
 @onready var inventory: Control = $".."
 
+@export var in_shop : bool
+
 @export var ring : Resource
 @onready var ring_texture: Sprite2D = $RingTexture
 @onready var ring_info: Label = $RingInfo
@@ -26,9 +28,15 @@ var ring_hovering = false
 
 var my_spawn_rings = false
 
+var spawn_rings_once = true
+
 func _process(_delta: float) -> void:
+	if in_shop == true and Game.shop_rolled == true and spawn_rings_once == true:
+		spawn_rings()
+		spawn_rings_once = false
+	
 	if my_spawn_rings == true:
-		spawn_rings()	
+		spawn_rings()
 	
 	
 	if ring_hovering == true:
@@ -52,7 +60,8 @@ func _process(_delta: float) -> void:
 		position = lerp(position, ring_default_position, ring_lerp_speed)
 
 func _ready() -> void:
-	spawn_rings()
+	if in_shop == false:
+		spawn_rings()
 
 func _on_area_2d_mouse_entered() -> void:
 	ring_hovering = true
@@ -60,11 +69,13 @@ func _on_area_2d_mouse_entered() -> void:
 		ring_texture.scale = ring_hover_scale
 		ring_info.visible = true
 
+
 func _on_area_2d_mouse_exited() -> void:
 	ring_hovering = false
 	if Game.dragging == false:
 		ring_texture.scale = ring_default_scale
 		ring_info.visible = false
+
 
 func _on_ring_hover_area_area_entered(area: Area2D) -> void:
 	switch_rings(area)
@@ -74,56 +85,56 @@ func switch_rings(area):
 		if area.owner != inventory:
 			if ring_inventory_slot == 0:
 				if area.owner.ring_inventory_slot == 1:
-					ring = Game.slot_2
-					Game.slot_2 = Game.slot_1
-					Game.slot_1 = ring
+					ring = Game.inv_slot_2
+					Game.inv_slot_2 = Game.inv_slot_1
+					Game.inv_slot_1 = ring
 				if area.owner.ring_inventory_slot == 2:
-					ring = Game.slot_3
-					Game.slot_3 = Game.slot_1
-					Game.slot_1 = ring
+					ring = Game.inv_slot_3
+					Game.inv_slot_3 = Game.inv_slot_1
+					Game.inv_slot_1 = ring
 				if area.owner.ring_inventory_slot == 3:
-					ring = Game.slot_4
-					Game.slot_4 = Game.slot_1
-					Game.slot_1 = ring
+					ring = Game.inv_slot_4
+					Game.inv_slot_4 = Game.inv_slot_1
+					Game.inv_slot_1 = ring
 			if ring_inventory_slot == 1:
 				if area.owner.ring_inventory_slot == 0:
-					ring = Game.slot_1
-					Game.slot_1 = Game.slot_2
-					Game.slot_2 = ring
+					ring = Game.inv_slot_1
+					Game.inv_slot_1 = Game.inv_slot_2
+					Game.inv_slot_2 = ring
 				if area.owner.ring_inventory_slot == 2:
-					ring = Game.slot_3
-					Game.slot_3 = Game.slot_2
-					Game.slot_2 = ring
+					ring = Game.inv_slot_3
+					Game.inv_slot_3 = Game.inv_slot_2
+					Game.inv_slot_2 = ring
 				if area.owner.ring_inventory_slot == 3:
-					ring = Game.slot_4
-					Game.slot_4 = Game.slot_2
-					Game.slot_2 = ring
+					ring = Game.inv_slot_4
+					Game.inv_slot_4 = Game.inv_slot_2
+					Game.inv_slot_2 = ring
 			if ring_inventory_slot == 2:
 				if area.owner.ring_inventory_slot == 0:
-					ring = Game.slot_1
-					Game.slot_1 = Game.slot_3
-					Game.slot_3 = ring
+					ring = Game.inv_slot_1
+					Game.inv_slot_1 = Game.inv_slot_3
+					Game.inv_slot_3 = ring
 				if area.owner.ring_inventory_slot == 1:
-					ring = Game.slot_2
-					Game.slot_2 = Game.slot_3
-					Game.slot_3 = ring
+					ring = Game.inv_slot_2
+					Game.inv_slot_2 = Game.inv_slot_3
+					Game.inv_slot_3 = ring
 				if area.owner.ring_inventory_slot == 3:
-					ring = Game.slot_4
-					Game.slot_4 = Game.slot_3
-					Game.slot_3 = ring
+					ring = Game.inv_slot_4
+					Game.inv_slot_4 = Game.inv_slot_3
+					Game.inv_slot_3 = ring
 			if ring_inventory_slot == 3:
 				if area.owner.ring_inventory_slot == 0:
-					ring = Game.slot_1
-					Game.slot_1 = Game.slot_4
-					Game.slot_4 = ring
+					ring = Game.inv_slot_1
+					Game.inv_slot_1 = Game.inv_slot_4
+					Game.inv_slot_4 = ring
 				if area.owner.ring_inventory_slot == 1:
-					ring = Game.slot_2
-					Game.slot_2 = Game.slot_4
-					Game.slot_4 = ring
+					ring = Game.inv_slot_2
+					Game.inv_slot_2 = Game.inv_slot_4
+					Game.inv_slot_4 = ring
 				if area.owner.ring_inventory_slot == 2:
-					ring = Game.slot_3
-					Game.slot_3 = Game.slot_4
-					Game.slot_4 = ring
+					ring = Game.inv_slot_3
+					Game.inv_slot_3 = Game.inv_slot_4
+					Game.inv_slot_4 = ring
 			
 			my_ring_inventory_slot = area.owner.ring_inventory_slot
 			area.owner.ring_inventory_slot = ring_inventory_slot
@@ -135,18 +146,32 @@ func switch_rings(area):
 			area.owner.my_spawn_rings = true
 
 func spawn_rings():
-	if ring_inventory_slot == 0:
-		ring = Game.slot_1
-		position = ring_slot_1_position
-	elif ring_inventory_slot == 1:
-		ring = Game.slot_2
-		position = ring_slot_2_position
-	elif ring_inventory_slot == 2:
-		ring = Game.slot_3
-		position = ring_slot_3_position
-	elif ring_inventory_slot == 3:
-		ring = Game.slot_4
-		position = ring_slot_4_position
+	if in_shop == false:
+		if ring_inventory_slot == 0:
+			ring = Game.inv_slot_1
+			position = ring_slot_1_position
+		elif ring_inventory_slot == 1:
+			ring = Game.inv_slot_2
+			position = ring_slot_2_position
+		elif ring_inventory_slot == 2:
+			ring = Game.inv_slot_3
+			position = ring_slot_3_position
+		elif ring_inventory_slot == 3:
+			ring = Game.inv_slot_4
+			position = ring_slot_4_position
+	else:
+		if ring_inventory_slot == 0:
+			print("fa")
+			ring = Game.shop_slot_1
+			position = ring_slot_1_position
+		elif ring_inventory_slot == 1:
+			print("sh")
+			ring = Game.shop_slot_2
+			position = ring_slot_2_position
+		elif ring_inventory_slot == 2:
+			ring = Game.shop_slot_3
+			position = ring_slot_3_position
+
 
 	ring_default_position = position
 
