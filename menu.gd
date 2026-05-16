ecbede5ddf49f4ce2e169e4a5d28ae975e9f9f71
Grom Chavkerov <gromcha4@gmail.com>
2026-05-15 17:23:12 -1000

    Update menu.gd

--- menu.gd
+++ menu.gd
@@ -1,16 +1,14 @@
 extends Control
 
-const INVENTORY = preload("uid://ddekmjgy26nxc")
-const KNIFE_GAME = preload("uid://dvdk0ehoc50u2")
-const SHOP = preload("uid://dncohmuk8arq2")
+
 
 func _on_knife_game_button_pressed() -> void:
-	get_tree().change_scene_to_packed(KNIFE_GAME)
+	get_tree().change_scene_to_file(Game.KNIFE_GAME)
 
 
 func _on_coin_shop_button_pressed() -> void:
-	get_tree().change_scene_to_packed(SHOP)
+	get_tree().change_scene_to_file(Game.SHOP)
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("inventory"):
-		get_tree().change_scene_to_packed(INVENTORY)
+		get_tree().change_scene_to_file(Game.INVENTORY)
